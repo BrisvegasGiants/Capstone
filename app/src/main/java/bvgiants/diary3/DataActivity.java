0dfbff710acf578289a268781a27f332c4871758
atulic <atulic95@gmail.com>
2016-04-25 16:28:44 +1000

    started maps activity

--- app/src/main/java/bvgiants/diary3/DataActivity.java
+++ app/src/main/java/bvgiants/diary3/DataActivity.java
@@ -1,11 +1,15 @@
 package bvgiants.diary3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.Map;
 
 public class DataActivity extends AppCompatActivity {
 
@@ -13,14 +17,19 @@ public class DataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // setSupportActionBar(toolbar);
+
+        Button buttonMaps = (Button) findViewById(R.id.buttonMaps);
+        buttonMaps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent startMaps = new Intent(DataActivity.this, Map.class);
+                startActivity(startMaps);
             }
         });
     }
 
-}
+
+
+        }
