3acb333b90f43e6465508a05fb7a32679303ff89
taylorbeer <taylorbeer1993@outlook.com>
2016-05-09 21:12:42 +1000

    Front End Additions
    Front end work has been completed. Back end work will be completed
    during tomorrow group programming session. Dylan has confirmed this to
    be the case as some aspects of current backend functionality in other
    options may be revised as we go though code tomorrow. One of my
    particular sections is also heavily reliant on Dylans pedometer
    function. Dylan has signed these changes off as well.

--- app/src/main/java/bvgiants/diary3/SettingsActivity.java
+++ app/src/main/java/bvgiants/diary3/SettingsActivity.java
@@ -1,6 +1,8 @@
 package bvgiants.diary3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +23,12 @@ public class SettingsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
-}
+    // Takes user to selected screen(Activity)
+    public void openProfile(View view){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }}
